a1b226c5c9ecb361a3a2d8c50ce46daf07232506
Sakshi Kulkarni <64360642+Sakshikulkarni880@users.noreply.github.com>
2021-07-23 00:40:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function checkForWin() {
       for (let i=0; i < squares.length; i++) {
         if (squares[i].innerHTML == 2048) {
-          resultDisplay.innerHTML = 'You WIN'
+          resultDisplay.innerHTML = 'You WIN!!!'
           document.removeEventListener('keyup', control)
           setTimeout(() => clear(), 3000)
         }
@@ -241,7 +241,7 @@ document.addEventListener('DOMContentLoaded', () =>  {
         }
       }
       if (zeros === 0) {
-        resultDisplay.innerHTML = 'You LOSE'
+        resultDisplay.innerHTML = 'You LOSE!!!'
         document.removeEventListener('keyup', control)
         setTimeout(() => clear(), 3000)
       }
